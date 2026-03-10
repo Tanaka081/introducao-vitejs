aadc0f8c2586b6b3aa9f7dcc0d5737dea9d67103
Tanaka081 <teustanaka2008@gmail.com>
2026-03-10 10:54:41 -0300

    lala

---
+++ src/pages/exemplos/ex-01/index.jsx
@@ -0,0 +1,10 @@
+function Exemplo01() {
+    return (
+        <>
+            <h1>Exemplo 1</h1>
+            <p>Componente básico para redenrizar tela com React</p>
+        </>
+    )   
+}
+
+export default Exemplo01;
