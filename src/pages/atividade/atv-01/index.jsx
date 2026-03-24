772cced64e4cabe11cffd275bd3121e7cf5032a8
Tanaka081 <teustanaka2008@gmail.com>
2026-03-24 10:55:33 -0300

    24/03/26

--- src/pages/atividade/atv-01/index.jsx
+++ src/pages/atividade/atv-01/index.jsx
@@ -1,10 +1,10 @@
-import './index.css'
+import styles from './index.module.css'
 
 function Atividade01() {
     return (
         <>
-            <h1 className='atv'>Atividade 1</h1>
-            <p className='p_atv'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae nisi voluptatem exercitationem autem eveniet laborum consectetur esse earum quidem quas. Minima est beatae, dicta perferendis cumque nihil deserunt animi facere!</p>
+            <h1 className={styles.atv}>Atividade 1</h1>
+            <p className={styles.p_atv}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae nisi voluptatem exercitationem autem eveniet laborum consectetur esse earum quidem quas. Minima est beatae, dicta perferendis cumque nihil deserunt animi facere!</p>
         </>
     )   
 }
