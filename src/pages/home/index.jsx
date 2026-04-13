f282cde1dc64ec4a1d4d0abc5b2b917104ac23cd
Tanaka081 <teustanaka2008@gmail.com>
2026-04-13 10:56:27 -0300

    13/04/26

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -11,11 +11,13 @@ function Home() {
 
                 <Link to="/exemplo/1">Exemplo 1 - Componente básico</Link>
                 <Link to="/exemplo/2">Exemplo 2 - Uso de estilização com module</Link>
+                <Link to="/exemplo/3">Exemplo 3 - Botão</Link>
 
             </div>
             <div className={styles.containerItens}>
                 <h2>Atividades</h2>
                 <Link to="/atividade/1">Atividade 1 - Componente básico</Link>
+                    <Link to="/atividade/2">Atividade 2 - Contador Botao</Link>
             </div>
         </div>
     )
